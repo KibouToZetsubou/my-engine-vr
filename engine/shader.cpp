345e154a6bfaa1ad25b2ca066c468912e95f04d0
JollyCookie2000 <xam_comico@simplelogin.co>
2024-04-07 19:31:04 +0200

    Made uniforms for shaders configurable

--- engine/shader.cpp
+++ engine/shader.cpp
@@ -1,6 +1,11 @@
 #include "shader.hpp"
 
+#include <iostream>
+#include <map>
+#include <string>
+
 #include <GL/glew.h>
+#include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 LIB_API Shader::Shader(const std::string& vertex_shader_source, const std::string& fragment_shader_source)
@@ -19,7 +24,7 @@ void LIB_API Shader::render(const glm::mat4 world_matrix) const
 {
     glUseProgram(this->program_id);
 
-    // TODO: Make this dynamic
+    // TODO: Make the camera settings and the screen ratio configurable
     // Projection matrix
     const glm::mat4 projection_matrix = glm::perspective(glm::radians(90.0f), 1.333333333333333f, 0.01f, 1000.0f);
     const int uniform_location_matrix = glGetUniformLocation(this->program_id, "projection_matrix");
@@ -33,6 +38,27 @@ void LIB_API Shader::render(const glm::mat4 world_matrix) const
     const glm::mat4 inverse_transpose_view = glm::inverse(glm::transpose(world_matrix));
     const int uniform_location_inverse_transpose_view = glGetUniformLocation(this->program_id, "inverse_transpose_view");
     glUniformMatrix4fv(uniform_location_inverse_transpose_view, 1, GL_FALSE, glm::value_ptr(inverse_transpose_view));
+
+    for (auto i = this->floats.begin(); i != this->floats.end(); ++i)
+    {
+        // TODO: Handle case where the uniform in the map does not exists in the shader.
+        const int uniform_location = glGetUniformLocation(this->program_id, i->first.c_str());
+        glUniform1f(uniform_location, i->second);
+    }
+
+    for (auto i = this->ints.begin(); i != this->ints.end(); ++i)
+    {
+        // TODO: Handle case where the uniform in the map does not exists in the shader.
+        const int uniform_location = glGetUniformLocation(this->program_id, i->first.c_str());
+        glUniform1i(uniform_location, i->second);
+    }
+
+    for (auto i = this->vec3s.begin(); i != this->vec3s.end(); ++i)
+    {
+        // TODO: Handle case where the uniform in the map does not exists in the shader.
+        const int uniform_location = glGetUniformLocation(this->program_id, i->first.c_str());
+        glUniform3fv(uniform_location, 3, glm::value_ptr(i->second));
+    }
 }
 
 bool LIB_API Shader::compile()
@@ -76,4 +102,26 @@ bool LIB_API Shader::compile()
     glDeleteShader(fragment_shader_id);
 
     return true;
+}
+
+void LIB_API Shader::clear_uniforms()
+{
+    this->floats.clear();
+    this->ints.clear();
+    this->vec3s.clear();
+}
+
+void LIB_API Shader::set_float(const std::string name, const float value)
+{
+    this->floats[name] = value;
+}
+
+void LIB_API Shader::set_int(const std::string name, const int value)
+{
+    this->ints[name] = value;
+}
+
+void LIB_API Shader::set_vec3(const std::string name, const glm::vec3 value)
+{
+    this->vec3s[name] = value;
 }
--- engine/shader.hpp
+++ engine/shader.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <map>
 #include <string>
+
+#include <glm/glm.hpp>
 
 #include "common.hpp"
 #include "object.hpp"
-
-// TODO: Add setters for uniforms (ex: set_int(name, balue) )
 
 class LIB_API Shader : public Object
 {
@@ -15,10 +16,22 @@ public:
 
     void render(const glm::mat4 world_matrix) const override;
 
+    // TODO: Compile the shader in the constructor.
+    // TODO: Throw an exception if initialization fails.
     bool compile();
+
+    void clear_uniforms();
+    void set_float(const std::string name, const float value);
+    void set_int(const std::string name, const int value);
+    void set_vec3(const std::string name, const glm::vec3 value);
 protected:
+    // TODO: Do not store the source code.
     std::string vertex_shader_source;
     std::string fragment_shader_source;
 
     unsigned int program_id;
+
+    std::map<std::string, float> floats;
+    std::map<std::string, int> ints;
+    std::map<std::string, glm::vec3> vec3s;
 };
